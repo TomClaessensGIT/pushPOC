1461167dbfc467b3558716dec99b6b40e47c146c
Tom Claessens <tom.claessens@student.kdg.be>
2020-05-13 14:34:49 +0200

    setup

---
+++ src/views/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form action="/quotes" method="POST">
+      <input type="text" placeholder="name" name="name">
+      <input type="text" placeholder="quote" name="quote">
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
+<script>
+  var OneSignal = window.OneSignal || [];
+    OneSignal.push(function () {
+      OneSignal.init({
+        appId: "${YOUR_APP_ID}",
+        autoRegister: false,
+        notifyButton: {
+          enable: true,
+        },
+      });
+    });
+</script>
+<script>
+
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  methods:{
+
+  }
+}
+</script>
